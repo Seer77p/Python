eed4567845d98a742ba32b93764f8df37939561b
Seer77p <seer7703@mail.ru>
2023-02-06 17:59:03 +0300

    dz-4.1-2

--- DZ/dz4/dz4.py
+++ DZ/dz4/dz4.py
@@ -2,11 +2,48 @@
 # Вычислить число c заданной точностью d
 # Пример:
 # - при $d = 0.001, π = 3.141.$    $10^{-1} ≤ d ≤10^{-10}$
+# divider=1
+# pi=0
+# line=int(input('Введите длину числа ПИ после запятой: '))
+# for i in range(1000000):
+#     if i%2==0:
+#         pi+=4/divider
+#     else:
+#         pi-=4/divider
+#     divider+=2
+# print('Число пи = ', round((pi),line))
 
+
+# pi_1 = 3
+# plus_minus=1
+# for i in range(1, 1000000):
+#     pi_1 += (plus_minus*4) / ((i + i)*(i + i + 1)*(i + i + 2))
+#     plus_minus*=-1
+# print('Число пи = ', round((pi_1), line))
 
 #dz-4.2
 # Задайте натуральное число N. Напишите программу, 
 # которая составит список простых множителей числа N.
+
+numbers=int(input('Введите натуральное число: '))
+number_list=list()
+while numbers!=1:
+    if numbers%2==0:
+        number_list.append(2)
+        numbers//=2
+    elif numbers%3==0:
+         number_list.append(3)
+         numbers//=3
+    elif numbers%5==0:
+         number_list.append(5)
+         numbers//=5
+    elif numbers % 7 == 0:
+         number_list.append(7)
+         numbers//=7
+    else:
+        number_list.append(numbers)
+        numbers//=numbers
+    print(number_list, end='')
 
 #dz-4.3
 # Задайте последовательность чисел. Напишите программу, 
