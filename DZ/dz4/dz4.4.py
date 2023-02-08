295422f5fab0d758504b75751c3788afd966bbb4
Seer77p <seer7703@mail.ru>
2023-02-08 17:51:54 +0300

    Решение задач

--- DZ/dz4/dz4.4.py
+++ DZ/dz4/dz4.4.py
@@ -1,48 +1,31 @@
-#dz-4.4
-# Задана натуральная степень k. Сформировать случайным 
-#образом список коэффициентов (значения от 0 до 100) 
-#многочлена и записать в файл многочлен степени k.
+# dz-4.4
+# Задана натуральная степень k. Сформировать случайным
+# образом список коэффициентов (значения от 0 до 100)
+# многочлена и записать в файл многочлен степени k.
 # Пример: - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
-
 from random import randint
 
-print('Введите натуральную степень k:')
-k = int(input())
-
-
-def write_file(st):
-    with open('Task04.txt', 'w') as data:
-        data.write(st)
-
-
-def create_list(k):
-    list = []
-    for i in range(k + 1):
-        list.append(randint(0, 101))
-    return list
-
-
-def create_str(sp):
-    list = sp[::-1]
-    wr = ''
-    if len(list) < 1:
-        wr = 'x = 0'
-    else:
-        for i in range(len(list)):
-            if i != len(list) - 1 and list[i] != 0 and i != len(list) - 2:
-                wr += f'{list[i]}x^{len(list) - i - 1}'
-                if list[i + 1] != 0:
-                    wr += ' + '
-            elif i == len(list) - 2 and list[i] != 0:
-                wr += f'{list[i]}x'
-                if list[i + 1] != 0:
-                    wr += ' + '
-            elif i == len(list) - 1 and list[i] != 0:
-                wr += f'{list[i]} = 0'
-            elif i == len(list) - 1 and list[i] == 0:
-                wr += ' = 0'
-    return wr
-
-
-koef = create_list(k)
-write_file(create_str(koef))
+k = int(input('Введите коэффициент k: '))
+my_list = list()
+for i in range(k + 1):
+    my_list.append(randint(1, 101))
+polynomial = ''
+if k < 1:
+    polynomial = 'x = 0'
+else:
+    for i in range(k+1):
+        if i != k-1 and my_list[i] != 0 and i != k:
+            polynomial += f'{my_list[i]}x^{k - i}'
+            if my_list[i] != 0:
+                polynomial += ' + '
+        elif i == k-1 and my_list[i] != 0:
+            polynomial += f'{my_list[i]}x'
+            if my_list[i] != 0:
+                polynomial += ' + '
+        elif i == k and my_list[i] != 0:
+            polynomial += f'{my_list[i]} = 0\n\n'
+        elif i == k and my_list[i] == 0:
+            polynomial += ' = 0'
+with open('dz4.5.txt', 'w') as file:
+    file.write(polynomial)
+print(polynomial)
