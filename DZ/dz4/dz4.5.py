4be9c2e26c53c00cbff57a68195745fa4e646179
Seer77p <seer7703@mail.ru>
2023-02-09 17:32:53 +0300

    4,5

--- DZ/dz4/dz4.5.py
+++ DZ/dz4/dz4.5.py
@@ -1,26 +1,31 @@
-#dz-4.5
-# Даны два файла, в каждом из которых находится запись многочлена. 
+# dz-4.5
+# Даны два файла, в каждом из которых находится запись многочлена.
 # Задача - сформировать файл, содержащий сумму многочленов.
-a=[1,2,3,4,5,6,7,8,9,10,11]
-b=a[::-1]
-#c=str(a^b)
-if b==chr(94):
-    print(b)
-print(b,chr(94))
-print(ord('^'))
-print(chr(42))
+# a=[1,2,3,4,5,6,7,8,9,10,11]
+# b=a[::-1]
+# #c=str(a^b)
+# if b==chr(94):
+#     print(b)
+# print(b,chr(94))
+# print(ord('^'))
+# print(chr(42))
 
 import re
 
-q="11x ^ 5 + 39x ^ 4 - 81x ^ 3 + 86x ^ 2 + 22x + 60 = 0"
-#q="123+ 321- 456+554- 587 +6587 -854"
-#q=q.replace(" ", "")
-k=0
-wplus=q.split('+')
+q = "11x ^ 5 + 39x ^ 4 - 81x ^ 3 + 86x ^ 2 + 22x + 60 = 0"
 
-# for i in range(len(q)):
-#     if q[i]=='+':
-#         k=q[i-1]
-#         wplus=q[i]
-print(wplus, end='')  
+q1 = q.replace(" ", "").replace("^", "")
+q1 = q1[:-2]
+# q_list = q[::-1]
+qpl = list()
+qlist = list(q1)
+print(qlist, '\n', len(qlist), '\n')
 
+for i in range(len(qlist)):
+    if (qlist[i] == "+") or (qlist[i] == "-"):
+        # if qlist[i-1] != 'x':
+        #     print(i-1, end='')
+        #     qlist.pop(i-1)
+
+
+#print(q1, '\n', qlist)
