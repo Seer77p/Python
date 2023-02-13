6c6efcce71868c2caa8ca0d7621789e9b048f1be
Seer77p <seer7703@mail.ru>
2023-02-13 17:19:04 +0300

    dz5.3

--- DZ/dz5/dz5-3krest.py
+++ DZ/dz5/dz5-3krest.py
@@ -1,2 +1,11 @@
 #dz 5.3
 #Создайте программу для игры в ""Крестики-нолики"".
+size=3
+playing_field=[[1,2,3],[4,5,6],[7,8,9]]
+for i in range(0, len(playing_field)):
+    for j in range(0, len(playing_field)):
+        print(playing_field[i][j], end='  ')
+    print()
+    
+print(playing_field)
+    
