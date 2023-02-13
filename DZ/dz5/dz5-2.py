e20323083ef508a25bdf601d69f4e4db387bed4e
Seer77p <seer7703@mail.ru>
2023-02-13 16:48:11 +0300

    dz5.2

--- DZ/dz5/dz5-2.py
+++ DZ/dz5/dz5-2.py
@@ -7,117 +7,27 @@
 # Сколько конфет нужно взять первому игроку, чтобы забрать все конфеты у своего конкурента?
 # a) Добавьте игру против бота
 # b) Подумайте как наделить бота ""интеллектом""
-from random import randint, random
+from random import randint
+from funk import *
 
-player_bot = 'Соперник=компьютер'
-count = 0
-maxi=28
-victory_number = 57
-game_mode_selection = int(input(
-    'Введите режим игры(2 человека,  введите "2", либо человек против бота "1")'))
+
+game_mode_selection = int(game_mode())
 if game_mode_selection == 1:
     print('Игра человека против бота!')
-
-    complexity = int(input('Введите сложность 0 - легче, 1 сложнее:'))
+    complexity = int(difficulty_level())
     if complexity == 0:
-        player = str(input('Введите имя игрока: '))
-        candies = int(input('Введите общее количество конфет: '))
+        player = name()
+        candies = int(candies_numbers())
         print('Начнем игру\n')
-        while 0 <= candies:
-            count += 1
-            if count % 2 == 0:
-                print('Ход игрока', player, ': ')
-                quantity = int(input('Введите количество конфет от 1 до 28: '))
-            else:
-                print('Ход компьютера', player_bot, ': ')
-                
-                if candies <= maxi:
-                    quantity = randint(1, candies)
-                    print('Компьютер забрал', quantity, 'конфет')
-                else:
-                    quantity = randint(1, maxi+1)
-                    print('Компьютер забрал', quantity, 'конфет')
-            if candies-quantity < 0:
-                quantity = int(
-                    input('Вы ввели количество больше остатка, введите не больше чем осталось:'))
-            elif quantity == 0:
-                quantity = int(
-                    input('Вы ввели 0, введите от 1 до 28, но не больше остатка:'))
-            elif candies-quantity == 0:
-                if count % 2 == 0:
-                    print('Игрок', player, 'выиграл')
-                else:
-                    print('Игрок', player_bot, 'выиграл')
-                break
-            else:
-                candies -= quantity
-                print('Вы взяли ', quantity, 'конфет', 'осталось = ', candies)
-
+        base_game_computer_easy(player, candies)
     elif complexity == 1:
-        player = str(input('Введите имя игрока: '))
-        candies = int(input('Введите общее количество конфет: '))
+        player = name()
+        candies = int(candies_numbers())
         print('Начнем игру\n')
-        while 0 <= candies:
-            count += 1
-            if count % 2 == 0:
-                print('Ход игрока', player, ': ')
-                quantity = int(input('Введите количество конфет от 1 до 28: '))
-            else:
-                print('Ход компьютера', player_bot, ': ')
-                
-                if candies <= maxi:
-                    quantity = candies
-                    print('Компьютер забрал', quantity, 'конфет')
-                else:
-                    
-                    if victory_number > candies:
-                        quantity = victory_number-candies
-                    else:
-                        quantity = randint(1, maxi+1)
-                    print('Компьютер забрал', quantity, 'конфет')
-            if candies-quantity < 0:
-                quantity = int(
-                    input('Вы ввели количество больше остатка, введите не больше чем осталось:'))
-            elif quantity == 0:
-                quantity = int(
-                    input('Вы ввели 0, введите от 1 до 28, но не больше остатка:'))
-            elif candies-quantity == 0:
-                if count % 2 == 0:
-                    print('Игрок', player, 'выиграл')
-                else:
-                    print('Игрок', player_bot, 'выиграл')
-                break
-            else:
-                candies -= quantity
-                print('Вы взяли ', quantity, 'конфет', 'осталось = ', candies)
-
-
+        base_game_computer_difficult_level(player, candies)
 elif game_mode_selection == 2:
-    player_one = str(input('Введите имя первого игрока: '))
-    player_two = str(input('Введите имя второго игрока: '))
-    candies = int(input('Введите общее количество конфет: '))
+    player_one = name()
+    player_two = name()
+    candies = int(candies_numbers())
     print('Начнем игру\n')
-    while 0 <= candies:
-        count += 1
-        if count % 2 == 0:
-            print('Ход игрока', player_two, ': ')
-            quantity = int(input('Введите количество конфет от 1 до 28: '))
-        else:
-            print('Ход игрока', player_one, ': ')
-            quantity = int(input('Введите количество конфет от 1 до 28: '))
-
-        if candies-quantity < 0:
-            quantity = int(
-                input('Вы ввели количество больше остатка, введите не больше чем осталось:'))
-        elif quantity == 0:
-            quantity = int(
-                input('Вы ввели 0, введите от 1 до 28, но не больше остатка:'))
-        elif candies-quantity == 0:
-            if count % 2 == 0:
-                print('Игрок', player_two, 'выиграл')
-            else:
-                print('Игрок', player_one, 'выиграл')
-            break
-        else:
-            candies -= quantity
-            print('Вы взяли ', quantity, 'конфет', 'осталось = ', candies)
+    base_game_man(player_one, player_two, candies)
--- DZ/dz5/funk.py
+++ DZ/dz5/funk.py
@@ -1,30 +1,122 @@
-def name():
-    player = str(input('Имя игрока:'))
-    return player
+from random import randint
 
+maxi = 28
+victory_number = 57
+
+def name():
+    player = str(input('Имя игрока: '))
+    return player
 
 def candies_numbers():
     candies = int(input('Введите общее количество конфет: '))
     while candies == 0:
         print('Введенный размер списка', candies, 'не может быть нулевым')
-        candies = int(input('Введите размер списка: '))
+        candies = int(input('Введите количество конфет: '))
     return int(candies)
-
 
 def game_mode():
     game_mode_selection = int(input(
-        'Введите режим игры (2 человека,  введите "2", либо человек против бота "1")'))
+        'Введите режим игры ( 2 человека,  введите "2", либо человек против бота "1" )'))
     while 1 != game_mode_selection != 2:
-        game_mode_selection = int(input('Необходимо ввести либо "1" либо "2"'))
+        game_mode_selection = int(input('Необходимо ввести либо "1" либо "2" '))
     return game_mode_selection
 
 def difficulty_level():
-    complexity = int(input('Введите сложность 0 - легче, 1 сложнее:'))
+    complexity = int(input('Введите сложность 0 - легче, 1 сложнее: '))
     while 1 != complexity != 0:
-        complexity = int(input('Необходимо ввести либо "1" либо "0"'))
+        complexity = int(input('Необходимо ввести либо "1" либо "0" '))
     return complexity
 
+def checks_number(candies):
+    quantity = int(
+        input('Введите число в интервале от 1 до 28(включительно) = '))
+    while 1 > quantity or quantity > 28:
+        print('Вы ввели число ', quantity, 'это за границами условия')
+        quantity = int(input('Введите число от 1 до 28 = '))
+    while (candies-quantity) < 0:
+        print('Вы ввели количество больше остатка на = ', candies-quantity)
+        quantity = int(
+            input('Введите количество не больше чем осталось: = '))
+    return quantity
 
+def base_game_man(player_one, player_two, candies):
+    count = 0
+    count = randint(0, 1)
+    while 0 <= candies:
+        count += 1
+        if count % 2 == 0:
+            print('Ход игрока', player_one, ': ')
+            quantity = int(checks_number(candies))
+        else:
+            print('Ход игрока', player_two, ': ')
+            quantity = int(checks_number(candies))
+        if candies-quantity == 0:
+            if count % 2 == 0:
+                print('\nИгрок', player_one, 'выиграл\n')
+            else:
+                print('\nИгрок', player_two, 'выиграл\n')
+            break
+        else:
+            candies -= quantity
+            print('Вы взяли ', quantity, 'конфет', 'осталось = ', candies)
+
+def base_game_computer_easy(player, candies):
+    player_bot = 'Соперник=компьютер'
+    count = randint(0, 1)
+    while 0 <= candies:
+        count += 1
+        if count % 2 == 0:
+            print('Ход игрока', player, ': ')
+            quantity = int(checks_number(candies))
+        else:
+            print('Ход компьютера', player_bot, ': ')
+            if candies <= maxi:
+                quantity = randint(1, candies)
+                print('Компьютер забрал', quantity, 'конфет')
+            else:
+                quantity = randint(1, maxi+1)
+                print('Компьютер забрал', quantity, 'конфет')
+
+        if candies-quantity == 0:
+            if count % 2 == 0:
+                print('\nИгрок', player, 'выиграл\n')
+            else:
+                print('\nИгрок', player_bot, 'выиграл\n')
+            break
+        else:
+            candies -= quantity
+            print('Вы взяли ', quantity, 'конфет',
+                  'осталось = ', candies)
+
+def base_game_computer_difficult_level(player, candies):
+    player_bot = 'Соперник=компьютер'
+    count = randint(0, 1)
+    while 0 <= candies:
+        count += 1
+        if count % 2 == 0:
+            print('Ход игрока', player, ': ')
+            quantity = int(checks_number(candies))
+        else:
+            print('Ход компьютера', player_bot, ': ')
+            if candies <= maxi:
+                quantity = candies
+                print('Компьютер забрал', quantity, 'конфет')
+            else:
+                if victory_number > candies:
+                    quantity = candies-(maxi+1)
+                else:
+                    quantity = randint(1, maxi+1)
+                    print('Компьютер забрал', quantity, 'конфет')
+        if candies-quantity == 0:
+            if count % 2 == 0:
+                print('\nИгрок', player, 'выиграл\n')
+            else:
+                print('\nИгрок', player_bot, 'выиграл\n')
+            break
+        else:
+            candies -= quantity
+            print('Игрок взял ', quantity, 'конфет',
+                  'осталось = ', candies)
 
 
 if __name__ == '__main__':  # усли в качестве импорта не будт запускаться
